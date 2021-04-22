25990146288aa41bfdd9eaac748acb43f4cf0c04
sumonsheikh <sm.sumon117@gmail.com>
2021-04-22 20:21:57 +0600

    codeforeces word capitalization problem solution

---
+++ 281A/capitalize.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char s[1000],t[1000];
+    gets(s);
+    if(s[0]>='a'){
+        s[0] = s[0]-32;
+    puts(s);
+    }else{
+        puts(s);
+    }
+
+
+
+
+return 0;
+
+}
